56a8da47fec0ec62d3ffc8abfef559226aac9493
ThirtyCraZyLow <woramate.j@ku.th>
2018-05-03 02:56:14 +0700

    Finish api for tasks 👏

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,7 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Task as Task;
+
 class TaskController extends Controller
 {
+    public function index()
+    {
+        return Task::all();
+    }
+
+    public function show($id)
+    {
+        return Task::find($id);
+    }
+
+    public function store(Request $request)
+    {
+        Task::create($request->all());
+        return [
+            'message' => 'Create Task successful'
+        ];
+    }
     //
 }
--- app/Task.php
+++ app/Task.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     //
+    protected $fillable = [
+        'responsible_id',
+        'substitute_id',
+        'substituted_at',
+        'finished_at'
+    ];
 }
