2b925446a014c543a3aeba477d9cefb0febce3fc
ThirtyCraZyLow <woramate.j@ku.th>
2018-05-03 01:32:02 +0700

    Fix path with autherization grouping :smile:

--- routes/api.php
+++ routes/api.php
@@ -16,22 +16,26 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+// require auth route
 
+Route::post('/login', 'Auth\LoginController@login');
 
-//Department route
-Route::get('departments', 'DepartmentController@index');
-Route::get('departments/{id}', 'DepartmentController@show');
-Route::post('departments', 'DepartmentController@store');
+Route::group(['middleware' => ['auth:api']], function() {
 
-//User route
-Route::middleware('auth:api')->post('register', 'UserController@create');
-Route::middleware('auth:api')->put('user-department/{id}', 'UserController@change_department');
-Route::middleware('auth:api')->get('users', 'UserController@index');
-Route::middleware('auth:api')->put('user/{id}', 'UserController@change_detail');
-Route::middleware('auth:api')->get('user-by-role', 'UserController@get_user_by_role');
-Route::middleware('auth:api')->get('user', 'UserController@get_user');
-Route::post('login', 'Auth\LoginController@login');
+  // User routes
+  Route::post('/register', 'UserController@create');
+  Route::put('user-department/{id}', 'UserController@change_department');
+  Route::get('users', 'UserController@index');
+  Route::put('user/{id}', 'UserController@change_detail');
+  Route::get('user-by-role', 'UserController@get_user_by_role');
+  Route::get('user', 'UserController@get_user');
 
-//Relation route
-Route::middleware('auth:api')->post('relation', 'RelationController@store');
-Route::middleware('auth:api')->get('relations', 'RelationController@index');
+  // Department routes
+  Route::get('departments', 'DepartmentController@index');
+  Route::get('departments/{id}', 'DepartmentController@show');
+  Route::post('departments', 'DepartmentController@store');
+
+  // Relation routes
+  Route::post('relation', 'RelationController@store');
+  Route::get('relations', 'RelationController@index');
+});
